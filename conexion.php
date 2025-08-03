77de7804275f5e66ec29c2715c03a4248fda5e2f
frannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <franciscoscabrera2403@gmail.com>
2025-08-03 20:32:04 -0300

    htaccess para proteger el archivo de conexion y hacerlo privado.  arreglado el puerto de sql

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 $host = '127.0.0.1';      // o 'localhost'
-$puerto = '3307';         // puerto de xampp (antes era 3306, se cambio porque interferia con mariadb)
+$puerto = '3306';         // puerto de xampp (antes era 3307)
 $usuario = 'usuariopizzeria';        // nombre de usuario de MySQL
 $contrasena = 'V9p$Xz!rD7&bW#qLm@S3eT2^uNjF0yGhZwMkPvQa8RxCj';         // contraseña del usuario root de la base de datos
 $base_datos = 'pizzzaconmigo'; // nombre de la base de datos en phpmyadmin
