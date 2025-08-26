e615717f968e8dd77761115e2cfa384e1c31ec77
frannnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn <franciscoscabrera2403@gmail.com>
2025-08-26 19:22:17 -0300

    asd

--- index.php
+++ index.php
@@ -12,9 +12,27 @@ include 'conexion.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - PizzaConmigo</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+
+<header>
+
+<img id="logopizza" src="" alt="logo_pizzeria">
+
+
+</header>
+
+<div>
+
+
+</div>
+
+<footer>
+<p>desc. pizzeria etc contactos y eso uwu</p>
+
+</footer>
+
     
 </body>
 </html>
